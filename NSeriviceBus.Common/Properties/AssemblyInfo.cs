330e33d0ac56060c7ed6512ee7676ade5f74497f
Ripan Paul <ripan.paul@lateetud.com>
2017-11-24 21:01:44 +0530

    change the project name

--- NSeriviceBus.Common/Properties/AssemblyInfo.cs
+++ NSeriviceBus.Common/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Messages")]
+[assembly: AssemblyTitle("NSeriviceBus.Common")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Messages")]
+[assembly: AssemblyProduct("NSeriviceBus.Common")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
